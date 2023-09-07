3278f385ac5ad3e1e9a89d8ecfa0caec2d933d8a
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-09-07 11:53:46 +0300

    route Shops AddShop Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import './reset.scss';
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Header from './components/layout/Header';
 import LoginPage from './pages/LoginPage';
+import Shops from './pages/Shops';
+import AddShop from './pages/AddShop';
+import { useAuth } from './store/AuthProvider';
+import { Toaster } from 'react-hot-toast';
+import Register from './pages/Register';
 
 export default function App() {
+  const ctx = useAuth();
+  const { isUserLoggedIn } = ctx;
   return (
     <div>
+      <Toaster />
       <Header />
       <Routes>
-        <Route path='/login' element={<LoginPage />} />
+        <Route path='/' element={<Shops />} />
+        <Route path='/add-shop' element={<AddShop />} />
+        <Route path='/register' element={<Register />} />
+
+        <Route
+          path='/login'
+          element={isUserLoggedIn ? <Navigate to={'/'} /> : <LoginPage />}
+        />
       </Routes>
     </div>
   );
---
+++ src/pages/AddShop.jsx
@@ -0,0 +1,3 @@
+export default function AddShop() {
+  return <div>AddShop</div>;
+}
