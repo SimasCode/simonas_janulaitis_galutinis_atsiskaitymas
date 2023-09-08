7fe26e545afaa4032ac114e68ea0e4193413928d
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-09-08 11:55:50 +0300

    feature style update

--- src/components/layout/Feature.jsx
+++ src/components/layout/Feature.jsx
@@ -4,28 +4,34 @@ export default function Feature() {
     <div className='container featureContainer'>
       <div className='featureBlock'>
         <img src='../images/feature-images/trophy.svg' alt='' />
-        <div>
-          <h2>High Quality</h2>
-          <p>crafted from top materials</p>
+        <div className='featureTextContainer'>
+          <h2 className='featureTitle'>High Quality</h2>
+          <p className='featureText'>crafted from top materials</p>
         </div>
       </div>
 
       <div className='featureBlock'>
         <img src='../images/feature-images/guarantee.svg' alt='' />
-        <h2>Warranty Protection</h2>
-        <p>Over 2 years</p>
+        <div>
+          <h2 className='featureTitle'>Warranty Protection</h2>
+          <p className='featureText'>Over 2 years</p>
+        </div>
       </div>
 
       <div className='featureBlock'>
         <img src='../images/feature-images/shipping.svg' alt='' />
-        <h2>Free shipping</h2>
-        <p>Order over 150$</p>
+        <div>
+          <h2 className='featureTitle'>Free shipping</h2>
+          <p className='featureText'>Order over 150$</p>
+        </div>
       </div>
 
       <div className='featureBlock'>
         <img src='../images/feature-images/customer-support.svg' alt='' />
-        <h2>24/7</h2>
-        <p>Dedicated support</p>
+        <div>
+          <h2 className='featureTitle'>24/7 Support</h2>
+          <p className='featureText'>Dedicated support</p>
+        </div>
       </div>
     </div>
   );
