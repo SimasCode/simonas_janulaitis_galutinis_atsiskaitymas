418f2ac1fada8fc95806e0943511536311bb071c
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-09-08 14:39:04 +0300

    feature style change

--- src/components/layout/Feature.jsx
+++ src/components/layout/Feature.jsx
@@ -1,36 +1,36 @@
 import './feature.scss';
 export default function Feature() {
   return (
-    <div className='container featureContainer'>
-      <div className='featureBlock'>
+    <div className='container feature-container'>
+      <div className='feature-block'>
         <img src='../images/feature-images/trophy.svg' alt='' />
-        <div className='featureTextContainer'>
-          <h2 className='featureTitle'>High Quality</h2>
-          <p className='featureText'>crafted from top materials</p>
+        <div className='feature-text-container'>
+          <h2 className='feature-title'>High Quality</h2>
+          <p className='feature-text'>crafted from top materials</p>
         </div>
       </div>
 
-      <div className='featureBlock'>
+      <div className='feature-block'>
         <img src='../images/feature-images/guarantee.svg' alt='' />
         <div>
-          <h2 className='featureTitle'>Warranty Protection</h2>
-          <p className='featureText'>Over 2 years</p>
+          <h2 className='feature-title'>Warranty Protection</h2>
+          <p className='feature-text'>Over 2 years</p>
         </div>
       </div>
 
-      <div className='featureBlock'>
+      <div className='feature-block'>
         <img src='../images/feature-images/shipping.svg' alt='' />
         <div>
-          <h2 className='featureTitle'>Free shipping</h2>
-          <p className='featureText'>Order over 150$</p>
+          <h2 className='feature-title'>Free shipping</h2>
+          <p className='feature-text'>Order over 150$</p>
         </div>
       </div>
 
-      <div className='featureBlock'>
+      <div className='feature-block'>
         <img src='../images/feature-images/customer-support.svg' alt='' />
         <div>
-          <h2 className='featureTitle'>24/7 Support</h2>
-          <p className='featureText'>Dedicated support</p>
+          <h2 className='feature-title'>24/7 Support</h2>
+          <p className='feature-text'>Dedicated support</p>
         </div>
       </div>
     </div>
