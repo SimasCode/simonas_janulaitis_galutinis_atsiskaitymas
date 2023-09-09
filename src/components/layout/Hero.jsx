38fd0d682128a571db6456efa8b4bbd8dbc048e6
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-09-09 15:46:12 +0300

    hero images

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -1,8 +1,20 @@
 import './hero.scss';
 export default function Hero() {
   return (
-    <div className='container'>
-      <h1 className='heroTitle'>Hero Title</h1>
+    <div className='container hero-container'>
+      <img
+        className='hero-image'
+        src='../images/hero-images/hero-image1.jpg'
+        alt=''
+      />
+      <div>
+        <h1 className='hero-title'>Find shop which matches your wishes</h1>
+        <p className='hero-subtitle'>
+          Browse through our diverse range of meticulously selected sellers.
+          Designed to bring out your individuality and cater to your sense of
+          style.
+        </p>
+      </div>
     </div>
   );
 }
