b1ad3b64b81c3dd497e5520826e88651cc5d03a5
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-09-10 22:24:20 +0300

    SingleAddCard code update

--- src/pages/SingleAddCard.jsx
+++ src/pages/SingleAddCard.jsx
@@ -2,26 +2,30 @@ import { Link } from 'react-router-dom';
 import { useAuth } from '../store/AuthProvider';
 
 export default function SingleAddCard(props) {
-  const { id, title, userUid, description, imageUrl } = props.item;
+  const { id, userUid, shopName, imageUrl } = props.item;
   const ctx = useAuth();
-  return (
-    <li className={ctx.userUid === userUid ? '' : ''}>
-      <img className='' src={imageUrl} alt={title} />
-      <div className=''>
-        <h2 className=''>{title}</h2>
-      </div>
-      <p>{description}</p>
 
-      <div>
-        <Link className='' to={`/adds/${id}`}>
+  return (
+    <li
+      className={
+        userUid === ctx.userUid
+          ? 'shop-card-container user-border'
+          : 'shop-card-container'
+      }
+    >
+      <img className='shop-image' src={imageUrl} alt={`${shopName}-image`} />
+
+      <p className='shop-name'>{shopName}</p>
+
+      <div className='shop-buttons-container'>
+        <Link className='shop-button shop-read-button' to={`/adds/${id}`}>
           Read more
         </Link>
         {userUid === ctx.userUid && (
-          <button className='' onClick={props.onDelete}>
+          <button className='shop-button' onClick={props.onDelete}>
             Delete
           </button>
         )}
-        <p className=''>{id}</p>
       </div>
     </li>
   );
